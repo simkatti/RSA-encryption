60bac4996ee864eb1ffad57b6d625dccec3bcdf2
Simona <simonacallegari@outlook.com>
2025-04-03 17:28:32 +0300

    modifying tests

--- src/tests/test_decryption.py
+++ src/tests/test_decryption.py
@@ -9,29 +9,29 @@ class TestDecryption(unittest.TestCase):
         self.message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in'
         self.symbols = '!?=)&%€äöå023546'
         
+        msg_in_bytes = self.message.encode('utf-8')
+        self.msg_int = int.from_bytes(msg_in_bytes, 'big')
+        
+        symbols_in_bytes = self.symbols.encode('utf-8')
+        self.symbols_int = int.from_bytes(symbols_in_bytes, 'big')
+        
     def test_int_to_text(self):
-        m_in_bytes = self.message.encode("utf-8")
-        m = int.from_bytes(m_in_bytes, "big")
-        s_in_bytes = self.symbols.encode("utf-8")
-        s = int.from_bytes(s_in_bytes, "big")
-        message = self.d.int_to_text(m)
-        symbols = self.d.int_to_text(s)
+        message = self.d.int_to_text(self.msg_int)
+        symbols = self.d.int_to_text(self.symbols_int)
         
         self.assertEqual(message, self.message)
         self.assertEqual(symbols, self.symbols)
     
-    def tetst_decryption(self):
+    def test_decryption_message(self):
         c, pubkey, prikey = self.e.perform_encryption(self.message)
         n = prikey[0]
         d = prikey[1]
-        m = pow(c,d,n)
-        encrypted = self.d.perform_decryption(c,prikey)
-        self.assertEqual(encrypted,self.message)
+        decrypted = self.d.perform_decryption(c,prikey)
+        self.assertEqual(decrypted,self.message)
     
     def test_decryption_symbols(self):
         c, pubkey, prikey = self.e.perform_encryption(self.symbols)
         n = prikey[0]
         d = prikey[1]
-        m = pow(c,d,n)
-        encrypted = self.d.perform_decryption(c,prikey)
-        self.assertEqual(encrypted,self.symbols)
+        decrypted = self.d.perform_decryption(c,prikey)
+        self.assertEqual(decrypted,self.symbols)
